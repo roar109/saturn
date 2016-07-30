d84e79bcf6c8d9f7c0dcaef1a62bac89844630c5
Hector Mendoza <roar109@gmail.com>
2016-07-30 14:14:27 -0500

    add regex for content type

--- saturn.go
+++ saturn.go
@@ -11,10 +11,10 @@ import (
 func main() {
 	fmt.Println("Saturn is starting ...")
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)                                    //.Schemes("https")
-	router.HandleFunc("/message/{messageId}", GetMessageByIDHandler) //.Schemes("https")
-	router.HandleFunc("/job", JobRouteHandler).Methods("POST")       //.Schemes("https")
-	router.HandleFunc("/sjob", SJobRouteHandler).Methods("POST")     //.Schemes("https")
+	router.HandleFunc("/", Index)                                                                                         //.Schemes("https")
+	router.HandleFunc("/message/{messageId}", GetMessageByIDHandler)                                                      //.Schemes("https")
+	router.HandleFunc("/job", JobRouteHandler).Methods("POST").HeadersRegexp("Content-Type", "application/(text|json)")   //.Schemes("https")
+	router.HandleFunc("/sjob", SJobRouteHandler).Methods("POST").HeadersRegexp("Content-Type", "application/(text|json)") //.Schemes("https")
 	log.Fatal(http.ListenAndServe(":8088", router))
 }
 
